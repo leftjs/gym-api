ed3eb43e02d398361ca99931ed983ba9ba528e68
Jason.Zhang <lefttjs@gmail.com>
2016-04-28 18:13:16 +0800

    bargain add startdate mouth price

--- src/main/java/com/donler/gym/model/Bargain.java
+++ src/main/java/com/donler/gym/model/Bargain.java
@@ -3,6 +3,7 @@ package com.donler.gym.model;
 import io.swagger.annotations.ApiModelProperty;
 
 import javax.persistence.*;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import java.util.Date;
@@ -19,31 +20,32 @@ public class Bargain {
   @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
 
-  @ApiModelProperty(example = "小张")
+  @ApiModelProperty(example = "小张",required = true)
   @NotNull(message = "姓名不能为空")
   private String name;
 
 
-  @ApiModelProperty(example = "男", allowableValues = "男,女")
+  @ApiModelProperty(example = "男", allowableValues = "男,女",required = true)
   @NotNull(message = "性别不能为空")
   @Pattern(regexp = "^[男,女]$",message = "性别必须是[男,女]")
   private String sex;
 
-  @ApiModelProperty(example = "XiaoZhang")
+  @ApiModelProperty(example = "XiaoZhang",required = false)
   private String spell; // 拼音
 
   @NotNull(message = "身份证号码不能为空")
-  @ApiModelProperty(example = "321000000000000000")
+  @ApiModelProperty(example = "321000000000000000", required = true)
   private String idCard;
 
-  @ApiModelProperty(example = "18205253786")
+  @ApiModelProperty(example = "18205253786",required = true)
   @NotNull(message = "电话号码不能为空")
   private String phoneNumber;
 
 
+  @ApiModelProperty(required = true)
   @Temporal(value = TemporalType.DATE)
+  @NotNull(message = "生日不能为空")
   private Date birthday = new Date();
-
   public void setId(Long id) {
     this.id = id;
   }
@@ -51,6 +53,20 @@ public class Bargain {
   @NotNull(message = "地址不能为空")
   @ApiModelProperty(example = "枫林路485号")
   private String address;
+
+  @NotNull(message = "业务期限不能为空")
+  @Min(1)
+  @ApiModelProperty(example = "3",required = true)
+  private int monthCount;
+
+
+  @NotNull(message = "业务价格不能为空")
+  @ApiModelProperty(example = "1000",required = true)
+  private int price;
+
+  @NotNull(message = "起始时间不能为空")
+  @ApiModelProperty(required = true)
+  private Date startDate;
 
 
   public String getPdfPath() {
@@ -142,7 +158,31 @@ public class Bargain {
     this.salerId = salerId;
   }
 
-  public Bargain(String name, String sex, String spell, String idCard, String phoneNumber, Date birthday, String address) {
+  public String getPhoneNumber() {
+    return phoneNumber;
+  }
+
+  public void setPhoneNumber(String phoneNumber) {
+    this.phoneNumber = phoneNumber;
+  }
+
+  public int getMonthCount() {
+    return monthCount;
+  }
+
+  public void setMonthCount(int monthCount) {
+    this.monthCount = monthCount;
+  }
+
+  public int getPrice() {
+    return price;
+  }
+
+  public void setPrice(int price) {
+    this.price = price;
+  }
+
+  public Bargain(String name, String sex, String spell, String idCard, String phoneNumber, Date birthday, String address, int monthCount, int price, Date startDate, String pdfPath, Long salerId) {
     this.name = name;
     this.sex = sex;
     this.spell = spell;
@@ -150,6 +190,19 @@ public class Bargain {
     this.phoneNumber = phoneNumber;
     this.birthday = birthday;
     this.address = address;
+    this.monthCount = monthCount;
+    this.price = price;
+    this.startDate = startDate;
+    this.pdfPath = pdfPath;
+    this.salerId = salerId;
+  }
+
+  public Date getStartDate() {
+    return startDate;
+  }
+
+  public void setStartDate(Date startDate) {
+    this.startDate = startDate;
   }
 
   public Bargain() {
